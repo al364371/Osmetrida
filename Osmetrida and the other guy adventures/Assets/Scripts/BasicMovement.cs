2d2e72c9a06ce8bf0450cbcd3e4dc999353273ed
Iván Gomis <al361649@uji.es>
2018-11-07 15:02:38 +0100

    Steve básico
    Inclusión de el movimiento básico de Steve (movimiento y salto)

---
+++ Osmetrida and the other guy adventures/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private float jumpTimerCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    private Animator animator;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+	
+	void FixedUpdate ()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        //Animator Control
+
+        if (moveInput != 0)
+        {
+            animator.SetBool("isWalk", true);
+        }
+
+        else
+        {
+            animator.SetBool("isWalk", false);
+        }       
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0,0,0);
+        }
+        else if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0,180,0);
+        }
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimerCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimerCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimerCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+
+        //Animator Control
+
+        if (isGrounded)
+        {
+            animator.SetBool("isFly", false);
+        }
+        else
+        {
+            animator.SetBool("isFly", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("isJump", true);
+        }
+
+        else
+        {
+            animator.SetBool("isJump", false);
+        }
+    }
+}
