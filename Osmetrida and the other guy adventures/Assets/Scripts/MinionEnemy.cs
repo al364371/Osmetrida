c0e4128b145ad0fe042bf62569237e7d7c070ea7
Iván Gomis <al361649@uji.es>
2018-11-14 13:22:01 +0100

    IA añadida
    Super Inteligencia Artificial añadida

---
+++ Osmetrida and the other guy adventures/Assets/Scripts/MinionEnemy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionEnemy : MonoBehaviour {
+
+    private Animator animator;
+
+    public float hitDistance;
+    public Transform player;
+
+    public float wallColission;
+    float distance = 1f;
+    public float speed;
+
+    private bool movingRight = false;
+    public Transform groundDetection;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Physics2D.queriesStartInColliders = false;
+    }
+
+    void Update()
+    {
+
+        RaycastHit2D grounInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, -hitDistance);
+
+        RaycastHit2D wallInfo = Physics2D.Raycast(groundDetection.position, transform.right, -wallColission);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (grounInfo.collider == false || (wallInfo.collider == true && wallInfo.collider.tag != "Player"))
+        {
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                movingRight = true;
+            }
+        }
+
+        if (hitInfo.collider != null && hitInfo.collider.tag == "Player")
+        {
+            animator.SetBool("isShoot", true);
+        }
+        if (hitInfo.collider == null || (hitInfo.collider != null && hitInfo.collider.tag != "Player"))
+        {
+            animator.SetBool("isShoot", false);
+        }
+    }
+
+}
---
+++ Osmetrida and the other guy adventures/Assets/Scripts/RanjedEnemy.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RanjedEnemy : MonoBehaviour
+{
+    private Animator animator;
+
+    public float shootDistance;
+    public Transform player;
+
+    public float wallColission;
+    float distance = 1f;
+    public float speed;
+
+    private bool movingRight = false;
+    public Transform groundDetection;
+
+    private float timeBtwShoots;
+    public float startTimeBtwShoots;
+    public Transform projectile;
+
+    public Transform steveDetection;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Physics2D.queriesStartInColliders = false;
+    }
+
+    void Update () {
+
+        RaycastHit2D grounInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(steveDetection.position, transform.right, -shootDistance);
+
+        RaycastHit2D wallInfo = Physics2D.Raycast(groundDetection.position, transform.right, -wallColission);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            timeBtwShoots = 0.15f;
+        }
+
+        if (grounInfo.collider == false || (wallInfo.collider == true && wallInfo.collider.tag != "Player"))
+        {
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                movingRight = true;
+            }
+        }
+
+        if (hitInfo.collider != null && hitInfo.collider.tag == "Player")
+        {
+            animator.SetBool("isShoot", true);
+
+            if (timeBtwShoots <= 0)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShoots = startTimeBtwShoots;
+            }
+
+            else
+            {
+                timeBtwShoots -= Time.deltaTime;
+            }
+        }
+        if (hitInfo.collider == null || (hitInfo.collider != null && hitInfo.collider.tag != "Player"))
+        {
+            animator.SetBool("isShoot", false);
+        }
+    }
+}
