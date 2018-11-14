257c92390f80012dee5bd62d370a2a6020b42316
Juanluich <juanlucolas@gmail.com>
2018-11-14 17:31:18 +0100

    Menú UI

---
+++ Osmetrida and the other guy adventures/Assets/Scripts/Controlador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour {
+
+    public void CambiarEscena(string SampleScene){
+        print("Cambiando a la escena" + SampleScene);
+        SceneManager.LoadScene(SampleScene);
+        }
+   /* public void CambiarAContinuar(string PartidaContinuada)
+    {
+        print("Cambiando a la escena" + PartidaContinuada);
+        SceneManager.LoadScene(PartidaContinuada);
+    }
+    public void CambiarAOpciones(string Opciones)
+    {
+        print("Cambiando a la escena" + Opciones);
+        SceneManager.LoadScene(Opciones);
+    }
+    */
+    public void Salir() {
+        print("Saliendo del juego");
+        Application.Quit();
+        }
+}
