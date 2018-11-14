2621c78984278ebb049659466931f411e4ca5c02
JDex18 <44837818+JDex18@users.noreply.github.com>
2018-11-14 17:21:20 +0100

    Ataque y muerte de enemigos

---
+++ Osmetrida and the other guy adventures/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    public int vida;
+    public Animator animator;
+    public float timeBfrDie = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (vida == 0)
+        {
+            animator.Play("Die");
+            Destroy(gameObject, timeBfrDie);
+        }
+
+    }
+}
