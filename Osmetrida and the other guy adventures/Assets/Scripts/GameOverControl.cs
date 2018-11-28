7bb34ba2bcacdb350b85fdecef30c965180fc5dc
Juanluich <juanlucolas@gmail.com>
2018-11-28 16:54:41 +0100

    Escena Game Over

---
+++ Osmetrida and the other guy adventures/Assets/Scripts/GameOverControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverControl : MonoBehaviour
+{
+    public void CambiarGameOver(string GameOver)
+    {
+        print("Volviendo a " + GameOver);
+        SceneManager.LoadScene(GameOver);
+    }
+}
