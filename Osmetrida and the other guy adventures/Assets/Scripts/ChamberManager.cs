8ee22b1664690b16732d554291f436c3c639ff5c
joanely3000 <44837269+joanely3000@users.noreply.github.com>
2018-12-04 18:49:36 +0100

    añadido fade in y fade out
    Se ha añadido el script ChamberManager

---
+++ Osmetrida and the other guy adventures/Assets/Scripts/ChamberManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChamberManager : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator FadeIn()
+    {
+        anim.SetTrigger("end");
+        yield return new WaitForSeconds(0.5f);
+        //Linea para cambiar de zona de camaras
+    }
+
+    public IEnumerator FadeOut()
+    {
+        anim.SetTrigger("in");
+        yield return new WaitForSeconds(0.5f);
+        //Linea para cambiar de zona de camaras
+    }
+}
